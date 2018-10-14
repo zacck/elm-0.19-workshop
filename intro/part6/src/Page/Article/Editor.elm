c50c1da496936d8084c2ba099686e198775a5846
Zacck Osiemo <coderv63@gmail.com>
2018-10-14 07:17:14 +0200

    fix tags to strip out empty ones

--- intro/part6/src/Page/Article/Editor.elm
+++ intro/part6/src/Page/Article/Editor.elm
@@ -554,24 +554,10 @@ create (Trimmed form) cred =
 
 
 toTagList : String -> List String
-toTagList tagString =
-    {- 👉 TODO #2 of 2: add another |> to the end of this pipeline,
-       which filters out any remaining empty strings.
-
-       To see if the bug is fixed, visit http://localhost:3000/#/editor
-       (you'll need to be logged in) and create an article with tags that have
-       multiple spaces between them, e.g. "tag1     tag2     tag3"
-
-       If the bug has not been fixed, trying to save an article with those tags
-       will result in an error! If it has been fixed, saving will work and the
-       tags will be accepted.
-
-       💡 HINT: Here's how to remove all the "foo" strings from a list of strings:
-
-       List.filter (\str -> str == "foo") listOfStrings
-    -}
+toTagList tagString = 
     String.split " " tagString
         |> List.map String.trim
+        |> List.filter (\str -> str /= "")
 
 
 edit : Slug -> TrimmedForm -> Cred -> Http.Request (Article Full)
