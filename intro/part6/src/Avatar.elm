59530e254f758b20b51beb516800db155fa9d901
Zacck Osiemo <coderv63@gmail.com>
2018-10-14 07:16:41 +0200

    fix user avatar urls

--- intro/part6/src/Avatar.elm
+++ intro/part6/src/Avatar.elm
@@ -40,12 +40,11 @@ src (Avatar maybeUrl) =
 
 resolveAvatarUrl : Maybe String -> String
 resolveAvatarUrl maybeUrl =
-    {- 👉 TODO #1 of 2: return the user's avatar from maybeUrl, if maybeUrl actually
-       contains one. If maybeUrl is Nothing, return this URL instead:
-
-          https://static.productionready.io/images/smiley-cyrus.jpg
-    -}
-    ""
+    case maybeUrl of 
+        Just url ->
+            url 
+        Nothing ->
+            "https://static.productionready.io/images/smiley-cyrus.jpg"
 
 
 encode : Avatar -> Value
