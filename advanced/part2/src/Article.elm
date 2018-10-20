9d1be9ba8d8d553cfd412effea7467dad99d3c96
Zacck Osiemo <coderv63@gmail.com>
2018-10-20 11:43:16 +0200

    using constraint unification and converting decoders

--- advanced/part2/src/Article.elm
+++ advanced/part2/src/Article.elm
@@ -159,9 +159,8 @@ slug (Article internals _) =
 
 
 body : Article Full -> Body
-body _ =
-    "👉 TODO make this return the article's body"
-
+body (Article _ (Full abody)) =
+    abody
 
 
 -- TRANSFORM
@@ -181,8 +180,8 @@ mapAuthor transform (Article info extras) =
 
 
 fromPreview : Body -> Article Preview -> Article Full
-fromPreview _ _ =
-    "👉 TODO convert from an Article Preview to an Article Full"
+fromPreview abody (Article preview Preview) =
+    Article preview (Full abody)
 
 
 
@@ -200,7 +199,8 @@ fullDecoder : Maybe Cred -> Decoder (Article Full)
 fullDecoder maybeCred =
     Decode.succeed Article
         |> custom (internalsDecoder maybeCred)
-        |> required "body" "👉 TODO use `Body.decoder` (which is a `Decoder Body`) to decode the body into this Article Full"
+        -- Convert Decoder Body to Decoder Full
+        |> required "body" (Decode.map Full Body.decoder) 
 
 
 internalsDecoder : Maybe Cred -> Decoder Internals
